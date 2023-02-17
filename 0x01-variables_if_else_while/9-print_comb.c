058c5a14a6c1900aafeafad11303f69e545fc0b6
HinnMachache <bennyhinn253.bh@gmail.com>
2023-02-17 07:52:05 -0800

    Commit changes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = '0''; digit <= '8'; digit++)
+	{
+		putchar(digit);
+		putchar(',');
+		putchar(' ');
+	}
+	digit++;
+	putchar(digit);
+
+	return (0);
+}
