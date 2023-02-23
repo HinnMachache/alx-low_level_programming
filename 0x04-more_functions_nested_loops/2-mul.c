b5029ed4e83a47c5a82a601ddd283c15150a2185
HinnMachache <bennyhinn253.bh@gmail.com>
2023-02-23 02:12:16 -0800

    More functions and nested loop files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: Parameter
+ * @b: Parameter
+ * Return: Return the results
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int num, i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num + '0');
+			if (num == 14)
+				_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
