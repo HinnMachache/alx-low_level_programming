7f885eb10af924b68b282ad3671acc567d2485ab
HinnMachache <bennyhinn253.bh@gmail.com>
2023-03-09 10:08:25 -0800

    Recursions's

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,10 @@
  * _strlen_recursion - prints length of string
  * @s: String to be considered
  */
-
-void _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
 	else
-		return (1 _ _strlen_recursion(s _ 1));
+		return (1 + _strlen_recursion(s + 1));
 }
