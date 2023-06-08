b265595fc58d256cef6a396e9b242a599fd81d55
HinnMachache <bennyhinn253.bh@gmail.com>
2023-06-08 15:25:04 -0700

    Recursin files

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,11 +3,12 @@
  * _pow_recursion - returns a value raised ttoa power
  * @x: Base number
  * @y: Index number
+ * Return: Return power of a number
  */
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0 )
+	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
