73b0d4d2d9e98b98adcac04313ee945c951bf8e4
HinnMachache <bennyhinn253.bh@gmail.com>
2023-06-13 02:49:46 -0700

    Correction: Float like a butterfly, sting like a be

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,6 +19,6 @@ char *create_array(unsigned int size, char c)
 	if (charPointer == NULL)
 		return (NULL);
 	for (counter = 0; counter < size; counter++)
-		*charPointer = c;
+		charPointer = c;
 	return (charPointer);
 }
