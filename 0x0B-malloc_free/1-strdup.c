ca5ecc6c45fddd04e5612d49739890389a4b0fbe
HinnMachache <bennyhinn253.bh@gmail.com>
2023-06-13 03:36:48 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup- Duplicates a string
+ * @str: Source string to ve duplicated
+ * Return: pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	int lengthCopy, length = 0;
+	char *strPointer;
+
+	if (str == NULL)
+		return (NULL);
+	while (*(str + length != '\0'))
+		length++;
+	strPointer = malloc(length + 1);
+	if (strPointer == NULL)
+		return (NULL);
+	for (lengthCopy = 0; str(lengthCopy); lengthCopy++)
+		strPointer[lengthCopy] = str[lengthCopy];
+	return (strPointer);
+}
