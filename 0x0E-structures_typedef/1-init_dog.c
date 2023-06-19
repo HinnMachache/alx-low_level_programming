c905e086236ec00638572ca672c148d6839832e4
HinnMachache <bennyhinn253.bh@gmail.com>
2023-06-19 04:04:57 -0700

    Structures and Typedef in C

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,7 @@
  * @age: Age of dog struct
  * @owner: Dog owner
  */
-void init_dog(struct dog *d, char *name, float age, char owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
