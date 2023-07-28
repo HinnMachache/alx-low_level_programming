078c7afdc1b1d4dcfa816cec3998d63a8f007b46
HinnMachache <bennyhinn253.bh@gmail.com>
2023-07-28 09:11:11 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fD = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, length);
+	w = write(fD, text_content, length);
 	if (fD == -1 || w == -1)
 		return (-1);
 	close(fD);
